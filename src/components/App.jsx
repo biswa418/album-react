485eb2e14a3e170dc36bbcd9d20e22ead20f7ead
biswa418 <biswajeetsutar418@gmail.com>
2023-05-30 23:00:01 +0530

    delete button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,83 @@
 import { useEffect, useState } from 'react'
 import Loader from './Loader';
+import { delUrl, getUrl } from '../constant';
 
 function App() {
   const [loading, setLoading] = useState(true)
   const [albums, setAlbums] = useState([]);
+  const [error, showError] = useState(false);
+  let [message, showMessage] = useState('None');
 
   useEffect(() => {
     //api call
-    fetch('https://jsonplaceholder.typicode.com/albums')
+    fetch(getUrl)
       .then(response => response.json())
       .then((response) => setAlbums(response))
       .then(() => setLoading(false))
+      .catch((err) => {
+        setLoading(false);
+        showMessage(err);
+        showError(true);
+      })
   }, [])
+
+  const removeFromState = (id) => {
+    const newAlbums = albums.filter((album) => {
+      return album.id != id
+    })
+
+    setAlbums(newAlbums)
+  }
+
+  function handleDelete(e) {
+    e.preventDefault();
+    e.target.innerText = '-'
+
+    fetch(delUrl(e.target.value), {
+      method: 'DELETE',
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res))
+      .then(() => {
+        removeFromState(e.target.value)
+      })
+      .catch((err) => {
+        setLoading(false);
+        showMessage(err);
+        showError(true);
+      })
+  }
 
   return (
     <>
       {loading &&
         <Loader />
       }
+
+      {!loading && error &&
+        <>
+          <p className='card-container'>
+            {`Issue in loading.. check your internet connection. ${message}`}
+          </p>
+        </>
+      }
+
       {
-        !loading
+        !loading && !error
         &&
         <div className='albums'>
           <h2 className='flex'>
-            <div class="loader2"></div>
+            <div className="loader2"></div>
             Albums
           </h2>
+
           <div className='card-container'>
             {albums.map((album) => {
               return (
-                <div key={album.id} className='card'>
+                <div key={`album-${album.id}`} className='card'>
                   <span>{album.id}.</span>
                   <p>{album.title}</p>
+                  <button value={album.id} onClick={handleDelete}>X</button>
                 </div>
               )
             })}
---
+++ src/constant/index.js
@@ -0,0 +1,5 @@
+export const getUrl = `https://jsonplaceholder.typicode.com/albums`
+
+export function delUrl(id) {
+    return `https://jsonplaceholder.typicode.com/albums/${id}`
+}
